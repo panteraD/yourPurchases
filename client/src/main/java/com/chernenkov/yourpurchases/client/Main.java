b66e6b0676188d899ccc9ae02aa3729ea7891ad7
doller <chernenkov.andrei@Yandex.ru>
2015-12-18 21:00:35 +0300

    Polymer Vidgets fixed

--- client/src/main/java/com/chernenkov/yourpurchases/client/Main.java
+++ client/src/main/java/com/chernenkov/yourpurchases/client/Main.java
@@ -4,33 +4,16 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.http.client.*;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
-
-
+import com.vaadin.polymer.paper.widget.PaperButton;
 
 
 public class Main implements EntryPoint {
 
 
     public void onModuleLoad() {
-        RootPanel.get().add(new VaadinWidget());
-
-//        RequestBuilder builder =
-//                new RequestBuilder(RequestBuilder.GET, "/server/rest/records/double?string=" + URL.encodeQueryString("hello"));
-//
-//        builder.setCallback(new RequestCallback() {
-//            public void onResponseReceived(Request request, Response response) {
-//                Window.alert("response = " + response.getText());
-//            }
-//
-//            public void onError(Request request, Throwable exception) {
-//                Window.alert("error");
-//            }
-//        });
-//
-//        try {
-//            builder.send();
-//        } catch (RequestException e) {
-//            e.printStackTrace();
-//        }
+        
+        PaperButton button = new PaperButton("Press me!");
+        button.setRaised(true);
+        RootPanel.get().add(button);
     }
 }
